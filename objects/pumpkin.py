4ae8cbd9eb9ff072980b1f89c83913f39202851f
lsklemm <sklema.alen@gmail.com>
2022-04-19 18:51:17 +0300

    animation pumpkin jumping

--- objects/pumpkin.py
+++ objects/pumpkin.py
@@ -7,8 +7,13 @@ class Pumpkin(pygame.sprite.Sprite):
         self.screen = screen
 
         self.alive = True
+        self.stop = False
+
         self.image = pygame.image.load('img/pumpkin1.png')
         self.rect = self.image.get_rect(center=(200,500))
+        self.de_index = 0
+        self.max_time = 4
+        self.de_time = 0
 
     # update current PUMPKIN state
     def update(self):
@@ -16,5 +21,21 @@ class Pumpkin(pygame.sprite.Sprite):
             self.screen.blit(self.image, self.rect)
 
         else:
-            self.image = pygame.image.load('img/pumpkin9.png')
+            if not self.stop:
+                self.de_time += 1
+                if self.de_time == self.max_time:
+                    self.de_time = 0
+                    self.de_index += 1
+                    if self.de_index <= 8:
+                        path = 'img/pumpkin' + str(self.de_index) + '.png'
+                        self.image = pygame.image.load(path)
+                    elif self.de_index == 9:
+                        self.stop = True
+                        path = 'img/pumpkin9.png'
+                        self.image = pygame.image.load(path)
+
+            else:
+                path = 'img/pumpkin9.png'
+                self.image = pygame.image.load(path)
             self.screen.blit(self.image, self.rect)
+
