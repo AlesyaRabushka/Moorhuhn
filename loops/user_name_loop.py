486afd8efd79a36a67e97a1095c2e7a1126775f3
lsklemm <sklema.alen@gmail.com>
2022-04-25 19:51:43 +0300

    windows user name interface

--- loops/user_name_loop.py
+++ loops/user_name_loop.py
@@ -5,10 +5,14 @@ def user_name_loop(screen, sounds):
     user_name = '|'
     user_tick = 30
     box_width = True
+    bg4 = pygame.transform.scale(pygame.image.load('img/help_background/help_back.png'), (800, 600))
+    bg_rect = bg4.get_rect()
     # turn off CURSOR
     pygame.mouse.set_visible(False)
-
     while running:
+        screen.fill((90, 22, 45))
+        screen.blit(bg4, bg_rect)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -29,12 +33,15 @@ def user_name_loop(screen, sounds):
                     if box_width:
                         # type text SOUND
                         sounds.type_sound.play()
-                        user_name += event.unicode
+                        if len(user_name) != 12:
+                            user_name += event.unicode
                 user_name += '|'
 
-        screen.fill((254, 204, 153))
         b = Button(screen)
-        b.draw_text("Enter User Name", 20, 200, 200)
+        font2 = pygame.font.Font(None, 30)
+        b_d = font2.render('ENTER USER NAME', True, '#FFE80E')
+        b_surf_rect = b_d.get_rect(center=(540, 250))
+        screen.blit(b_d, b_surf_rect)
 
         user_tick -= 1
         if user_tick == 0:
@@ -45,18 +52,21 @@ def user_name_loop(screen, sounds):
 
         pygame.font.init()
         font = pygame.font.Font(None, 40)
-        user_name_surf = font.render(user_name, True, (255, 255, 255))
-        user_name_rect = pygame.Rect(200, 300, 400, 50)
-
+        user_name_surf = font.render(user_name, True, '#FFE80E')
+        user_name_rect = pygame.Rect(440, 294, 400, 50)
 
 
         # для регулирования длины
-        user_name_rect.w = max(200, user_name_surf.get_width() + 30)
-        if user_name_rect.w >= 700:
-            box_width = False
-        pygame.draw.rect(screen, 'lightskyblue3', user_name_rect, 2)
+        # user_name_rect.w = max(200, user_name_surf.get_width() + 30)
+        # if user_name_rect.w >= 700:
+        #     box_width = False
+        # pygame.draw.rect(screen, (230,100,230), user_name_rect, 2)
 
         screen.blit(user_name_surf, (user_name_rect.x + 10, user_name_rect.y + 10))
 
-        b.draw_text('Press ENTER to continue', 15, 200,400)
+        #b.draw_text('Press ENTER to continue', 15, 400,400)
+        font1 = pygame.font.Font(None, 30)
+        b_surf = font1.render('Press ENTER to continue', True, '#FFE80E')
+        b_surf_rect = b_surf.get_rect(center=(140,550))
+        screen.blit(b_surf,b_surf_rect)
         pygame.display.flip()
